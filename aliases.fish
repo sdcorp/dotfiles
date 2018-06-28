d34e9e3f45cb5c678e5ef1406789e563c84172fd
Dmitriy-Soldatov <dmitrsold@gmail.com>
2018-06-28 17:25:35 +0300

    add alias

---
+++ aliases.fish
@@ -0,0 +1,16 @@
+# tree
+alias sd 'tree -F -C --dirsfirst -L 1'
+alias sd2 'tree -F -C --dirsfirst -L 2'
+alias sd3 'tree -F -C --dirsfirst -L 3'
+
+# system
+alias c 'clear'
+alias cf 'touch'
+alias md 'mkdir'
+
+# npm
+alias n 'npm install'
+alias nid 'npm install -D'
+alias nrd 'npm run dev'
+alias nrs 'npm run start'
+alias nrb 'npm run build'
